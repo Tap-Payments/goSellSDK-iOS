bf001a197a7777705f690fd0ec499091391d8696
osama rabie <o.rabie@tap.company>
2020-02-19 10:39:45 +0200

    1- Fixing typo swapping min with max

--- goSellSDK/Core/API/Internal/Models/ExtraFee.swift
+++ goSellSDK/Core/API/Internal/Models/ExtraFee.swift
@@ -28,8 +28,8 @@ internal final class ExtraFee: AmountModificator {
         
         let type = try container.decode(AmountModificatorType.self, forKey: .type)
         let value = try container.decode(Decimal.self, forKey: .value)
-        let maxFee = try container.decodeIfPresent(Decimal.self, forKey: .minFee) ?? 0
-        let minFee = try container.decodeIfPresent(Decimal.self, forKey: .maxFee) ?? 0
+        let maxFee = try container.decodeIfPresent(Decimal.self, forKey: .maxFee) ?? 0
+        let minFee = try container.decodeIfPresent(Decimal.self, forKey: .minFee) ?? 0
         let currency = try container.decode(Currency.self, forKey: .currency)
         
         self.init(type: type, value: value, currency: currency,minFee:minFee, maxFee: maxFee)
