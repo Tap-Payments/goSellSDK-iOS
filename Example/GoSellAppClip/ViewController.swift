27ea0d9bda1574741eb4d5f8c554f3936c1d82f0
O Rabie <o.rabie@tap.company>
2020-11-20 06:36:09 +0200

    Added goSell clip target

---
+++ Example/GoSellAppClip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoSellAppClip
+//
+//  Created by Osama Rabie on 11/20/20.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
